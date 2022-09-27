19670f00a63034f992131bce24e392a730aa11ee
Camilo Alfonso <cabp393@gmail.com>
2022-09-27 17:15:03 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import Home from './pages/home'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <div className="App">
+    <main className="m-auto px-3 relative">
       <Navbar />
-
-      <h1 className="text-6xl text-primary">stable cupcakes</h1>
-      <h2>prompt</h2>
-      <p className="text-content">
-        lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos,
-        consectetur eaque? Atque dolor aliquam ullam recusandae nam sit tempore
-        eum molestiae dolorum asperiores odit id, tenetur, reprehenderit dolores
-        repudiandae odio, itaque obcaecati. Facere incidunt at cumque blanditiis
-        voluptate rerum? Neque non, soluta perferendis velit reiciendis
-        consequuntur tenetur recusandae inventore labore hic alias minima
-        doloribus maiores et eveniet a! Consequuntur, commodi in provident
-        perferendis sed quia laborum porro omnis aspernatur maiores laboriosam
-        iste labore aliquid suscipit necessitatibus explicabo ipsam consectetur,
-        aliquam corporis quae tempore? Earum cumque commodi sed amet,
-        voluptatibus, odit quo assumenda alias tenetur nostrum ea laboriosam
-        harum. Explicabo, itaque.
-      </p>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   )
 }
 
