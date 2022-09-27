11239c307a0b56192da04447b83d5f802acb602c
Camilo Alfonso <cabp393@gmail.com>
2022-09-27 17:16:31 -0300

    404 page added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+function NotFound() {
+  return (
+    <div className="NotFound">
+      <p className="text-content">
+        not found Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Minima enim, laboriosam saepe rerum ad reprehenderit quae accusamus
+        aspernatur praesentium a quia voluptas quibusdam optio ratione ab
+        incidunt quam necessitatibus quidem quo ipsam minus perferendis! Rerum
+        quaerat vel, distinctio mollitia nulla tempore tenetur qui quisquam,
+        delectus quidem voluptates numquam perspiciatis, saepe rem. Eaque
+        architecto modi animi odit praesentium vel earum possimus eum dolor
+        distinctio nisi maiores culpa vitae ut, obcaecati maxime in voluptates
+        aspernatur saepe sit cumque magnam quia. Laboriosam, necessitatibus.
+        Consequatur fugiat, nisi adipisci odio possimus explicabo praesentium
+        corrupti vitae est neque laboriosam voluptatibus similique molestiae,
+        eligendi, amet fuga? Repellendus!
+      </p>
+    </div>
+  )
+}
+
+export default NotFound
