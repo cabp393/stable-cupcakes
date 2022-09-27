4f0162c72ea1f8eeec439e3f9b03798beea2364a
Camilo Alfonso <cabp393@gmail.com>
2022-09-27 17:15:28 -0300

    navlink added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import { NavLink, useLocation } from 'react-router-dom'
+
 export function Navbar() {
+  const params = useLocation().pathname.slice(1)
+
   return (
-    <nav className="flex flex-cols justify-between bg-red-100">
-      <div>home</div>
-      <div className="text-detail">catalog &gt; realistic</div>
-      <div>login</div>
+    <nav className="flex flex-cols justify-between sticky top-0 py-3">
+      <NavLink to="/">
+        <div>home</div>
+      </NavLink>
+
+      <div className="text-detail">{params}</div>
+
+      <NavLink to="login">
+        <div>login</div>
+      </NavLink>
     </nav>
   )
 }
