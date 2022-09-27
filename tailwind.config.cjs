73cdad892753ee640974887adf8acfe29a27886b
Camilo Alfonso <cabp393@gmail.com>
2022-09-27 17:14:44 -0300

    extend colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: '#E82F6D',
-      content: '#a0a0a0',
-      detail: '#393939',
+    extend: {
+      colors: {
+        primary: '#E82F6D',
+        content: '#a0a0a0',
+        detail: '#393939',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
