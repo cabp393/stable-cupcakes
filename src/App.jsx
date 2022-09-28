165dd43fd339022c80e5d4b170fb4005f1e4627e
Camilo Alfonso <cabp393@gmail.com>
2022-09-28 17:02:58 -0300

    toaster and signup route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { Toaster } from 'react-hot-toast'
 import Home from './pages/home'
+import SignUp from './pages/SignUp'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
 
@@ -8,8 +10,20 @@ function App() {
   return (
     <main className="m-auto px-3 relative">
       <Navbar />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="signup" element={<SignUp />} />
         <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
