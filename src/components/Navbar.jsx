bf7c9816893f84786906a4a7d72cad052f67e184
Camilo Alfonso <cabp393@gmail.com>
2022-09-29 15:16:40 -0300

    logout btn added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,35 @@
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { supabase } from '../lib/supabase'
 
-export function Navbar() {
+export function Navbar({ session }) {
   const params = useLocation().pathname.slice(1)
+  const navigate = useNavigate()
+
+  async function logOut() {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      throw new error()
+    } else {
+      navigate('/')
+    }
+  }
 
   return (
     <nav className="flex flex-cols justify-between sticky top-0 py-3">
       <NavLink to="/">
         <div>home</div>
       </NavLink>
-
       <div className="text-detail">{params}</div>
 
-      <NavLink to="login">
-        <div>login</div>
-      </NavLink>
+      {session ? (
+        <div onClick={logOut} className="hover:cursor-pointer">
+          logout
+        </div>
+      ) : (
+        <NavLink to="login">
+          <div>login</div>
+        </NavLink>
+      )}
     </nav>
   )
 }
