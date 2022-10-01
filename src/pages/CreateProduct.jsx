ce4b76e9c5ba2f41b787a69344ba83dfee9a4511
Camilo Alfonso <cabp393@gmail.com>
2022-10-01 20:53:50 -0300

    add inputs for create product form

--- src/pages/CreateProduct.jsx
+++ src/pages/CreateProduct.jsx
@@ -1,11 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { supabase } from '../lib/supabase'
+import { Btn } from '../components/Btn'
 import toast from 'react-hot-toast'
 
 function CreateProduct() {
   const [uploading, setUploading] = useState(false)
   const [productUrl, setProductUrl] = useState(null)
   const [productImg, setProductImg] = useState(null)
+  const titleRef = useRef()
+  const slugRef = useRef()
+  const promptRef = useRef()
+  const stepsRef = useRef()
+  const samplerRef = useRef()
+  const cfg_scaleRef = useRef()
+  const seedRef = useRef()
 
   useEffect(() => {
     if (productUrl) downloadImage(productUrl)
@@ -55,11 +63,27 @@ function CreateProduct() {
     }
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    const product = {
+      title: titleRef.current.value,
+      slug: slugRef.current.value,
+      prompt: promptRef.current.value,
+      steps: stepsRef.current.value,
+      sampler: samplerRef.current.value,
+      cfg_scale: cfg_scaleRef.current.value,
+      seed: seedRef.current.value,
+      img_url: productUrl,
+    }
+
+    console.log(product)
+  }
+
   return (
     <section className="mt-20 lg:max-w-2xl m-auto">
-      <form className="flex flex-col gap-5 px-4">
+      <form className="flex flex-col gap-5 px-4" onSubmit={handleSubmit}>
         {productImg ? (
-          <img src={productImg} />
+          <img src={productImg} className="max-w-[50%] max-h-screen" />
         ) : (
           <input
             type="file"
@@ -67,8 +91,67 @@ function CreateProduct() {
             accept="image/*"
             onChange={uploadProductImg}
             disabled={uploading}
+            className="self-center file:mr-4 file:py-2 file:px-4
+            file:rounded-full file:border-0
+            file:text-sm file:font-semibold
+            file:bg-primary file:text-white hover:file:cursor-pointer"
           />
         )}
+
+        <label forhtml="title">title</label>
+        <input
+          type="text"
+          name="title"
+          ref={titleRef}
+          className="text-black px-4 py-2 rounded-full"
+        />
+
+        <label forhtml="slug">slug</label>
+        <input
+          type="text"
+          name="slug"
+          ref={slugRef}
+          className="text-black px-4 py-2 rounded-full"
+        />
+
+        <label forhtml="prompt">prompt</label>
+        <textarea
+          type="text"
+          name="prompt"
+          ref={promptRef}
+          className="text-black px-5 py-4 rounded-full resize-none overflow-hidden"
+        />
+
+        <label forhtml="steps">steps</label>
+        <input
+          type="text"
+          name="steps"
+          ref={stepsRef}
+          className="text-black px-4 py-2 rounded-full"
+        />
+        <label forhtml="sampler">sampler</label>
+        <input
+          type="text"
+          name="sampler"
+          ref={samplerRef}
+          className="text-black px-4 py-2 rounded-full"
+        />
+        <label forhtml="cfg_scale">cfg scale</label>
+        <input
+          type="text"
+          name="cfg_scale"
+          ref={cfg_scaleRef}
+          className="text-black px-4 py-2 rounded-full"
+        />
+        <label forhtml="seed">seed</label>
+        <input
+          type="text"
+          name="seed"
+          ref={seedRef}
+          className="text-black px-4 py-2 rounded-full"
+        />
+
+        <Btn title="submit" />
       </form>
     </section>
   )
