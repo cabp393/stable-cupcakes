c26a43c9ce0d85a2bd34eab2802d5ed525486ebb
Camilo Alfonso <cabp393@gmail.com>
2022-10-04 23:35:38 -0300

    add products page

---
+++ src/pages/Products.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase'
+
+function Products() {
+  const [productsList, setProductList] = useState([])
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  async function getProducts() {
+    try {
+      const { data, error } = await supabase.from('product').select('*')
+      if (error) {
+        throw error
+      }
+
+      for (const product of data) {
+        const imgUrl = await downloadImage(product.img_url)
+        product.img_url = imgUrl
+      }
+
+      setProductList(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function downloadImage(path) {
+    try {
+      const { data, error } = await supabase.storage
+        .from('products')
+        .download(path)
+      if (error) {
+        throw error
+      }
+
+      const url = URL.createObjectURL(data)
+      return url
+    } catch (error) {
+      toast.error('error downloading image: ', error.message)
+    }
+  }
+
+  return (
+    <div className="grid grid-cols-1 justify-items-center md:grid-cols-3 gap-5">
+      {productsList?.map(product => {
+        return (
+          <div key={product.id}>
+            <img
+              src={product.img_url}
+              alt={product.title}
+              className="rounded"
+            />
+            <h2>{product.title}</h2>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Products
