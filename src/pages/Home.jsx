c8b72bef78503b26d1d100f81416085af57e00fa
Camilo Alfonso <cabp393@gmail.com>
2022-10-16 23:18:19 -0300

    Add section and padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ function Home() {
   const heroImg = width > 480 ? '/full.avif' : '/mobile.avif'
 
   return (
-    <div className="flex flex-col">
+    <section className="flex flex-col">
       <img
         src={heroImg}
         alt="stable cupcake"
         className="absolute top-0 left-0 -z-10  w-full"
       />
 
-      <div className="flex flex-col self-center mt-80 w-full lg:max-w-3xl lg:mt-60">
+      <div className="flex flex-col self-center mt-80 w-full md:max-w-lg lg:max-w-4xl lg:mt-60 px-4">
         <h1 className="text-6xl lg:text-9xl">stable</h1>
         <h1 className="text-6xl lg:text-9xl text-right">cupcakes</h1>
       </div>
@@ -23,7 +23,7 @@ function Home() {
       >
         explore
       </NavLink>
-    </div>
+    </section>
   )
 }
 
