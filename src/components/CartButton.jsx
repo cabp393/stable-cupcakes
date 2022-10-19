3148886336a7251cc52230d6df19ef3b7cf2d3ed
Camilo Alfonso <cabp393@gmail.com>
2022-10-18 23:35:58 -0300

    Add cart total quantity

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,7 +1,12 @@
 import { NavLink } from 'react-router-dom'
+import { useDataContext } from '../hooks/useDataContext'
 import { IconCart } from './IconCart'
 
 export function CartButton() {
+  const { cart } = useDataContext()
+
+  if (cart.totalQuantity === 0) return
+
   return (
     <NavLink
       to={'/cart'}
@@ -9,7 +14,7 @@ export function CartButton() {
     >
       <IconCart size={30} />
       <div className="bg-primary absolute -top-4 right-10 px-4 py-1 rounded-full shadow-cart">
-        10
+        {cart.totalQuantity}
       </div>
     </NavLink>
   )
