23ccfee34c99f543766276fe45c80007eb480d51
Camilo Alfonso <cabp393@gmail.com>
2022-10-18 23:37:24 -0300

    Add actions and state

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,23 +1,89 @@
-export function CartProduct() {
+import { useState } from 'react'
+import { useDataContext } from '../hooks/useDataContext'
+
+export function CartProduct({ product }) {
+  const { setCart } = useDataContext()
+  const [inputQuantity, setInputQuantity] = useState(product.productQuantity)
+
+  const handleQuantity = e => {
+    const value = e.target.value
+    setInputQuantity(value)
+
+    setCart(prev => {
+      let findProduct = null
+      const filteredList = prev.products.filter(p => {
+        if (p.productId === product.productId) findProduct = p
+        return p.productId !== product.productId
+      })
+
+      const updateProduct = {
+        ...findProduct,
+        productQuantity: value,
+      }
+
+      const updateTotalPrice =
+        updateProduct.productQuantity * updateProduct.productPrice -
+        findProduct.productQuantity * findProduct.productPrice
+
+      const updateTotalQuantity =
+        updateProduct.productQuantity - findProduct.productQuantity
+
+      return {
+        ...prev,
+        totalQuantity: prev.totalQuantity + updateTotalQuantity,
+        totalPrice: prev.totalPrice + updateTotalPrice,
+        products: [...filteredList, updateProduct],
+      }
+    })
+  }
+
+  const handleRemove = () => {
+    setCart(prev => {
+      let findProduct = null
+      const removeProduct = prev.products.filter(p => {
+        if (p.productId === product.productId) findProduct = p
+        return p.productId !== product.productId
+      })
+
+      const updateTotalPrice =
+        findProduct.productPrice * product.productQuantity
+
+      return {
+        ...prev,
+        totalQuantity: prev.totalQuantity - findProduct.productQuantity,
+        totalPrice: prev.totalPrice - updateTotalPrice,
+        products: removeProduct,
+      }
+    })
+  }
+
   return (
     <article className="grid grid-cols-[20%_1fr_20%] gap-5 p-4 lg:px-80">
       <img
-        src="https://mpevthymxortshpjnugv.supabase.co/storage/v1/object/public/products/0.2898953730039151.png"
+        src={product.productImg}
         alt=""
         className="rounded-[10px] max-w-40 lg:max-h-40"
       />
 
       <div className="flex flex-col gap-2">
-        <div>product name</div>
-        <div className="text-content">$300</div>
+        <div>{product.productTitle}</div>
+        <div className="text-content">$ {product.productPrice}</div>
       </div>
 
       <div className="flex flex-col items-center gap-2">
         <input
-          value={'10'}
+          type="tel"
+          min={1}
+          value={inputQuantity}
+          onChange={handleQuantity}
           className="rounded w-[50%] text-black text-center"
         />
-        <div className="underline underline-offset-4 text-detail">remove</div>
+        <div
+          className="underline underline-offset-4 text-detail"
+          onClick={handleRemove}
+        >
+          remove
+        </div>
       </div>
     </article>
   )
