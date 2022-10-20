447ca4a3ec38e3849db01100c83031a3b19a4699
Camilo Alfonso <cabp393@gmail.com>
2022-10-20 02:16:08 -0300

    Change const variable and console error

--- src/utils/getImageDimensions.js
+++ src/utils/getImageDimensions.js
@@ -1,14 +1,14 @@
 const getImageDimensions = (img_url, setState) => {
   try {
-    const img = new Image()
-
+    let img = new Image()
     img.src = img_url
     img.onload = function () {
       setState({ img_url, img_width: this.width, img_height: this.height })
     }
     img = null
   } catch (error) {
-    console.log(error)
+    console.error(error)
   }
 }
+
 export default getImageDimensions
