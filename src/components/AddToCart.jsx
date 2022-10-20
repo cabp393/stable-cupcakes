85d1e432ba5bf93c12ab6aa7d464e02523920b2f
Camilo Alfonso <cabp393@gmail.com>
2022-10-20 02:18:14 -0300

    Change extract logic

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,32 +1,20 @@
 import { useDataContext } from '../hooks/useDataContext'
 import { CartButton } from './CartButton'
 
-function AddToCart({ productDetails: { steps, id, title, img_url } }) {
-  const { cart, setCart } = useDataContext()
+function AddToCart({ productDetails }) {
+  const { cart, addProductToCart } = useDataContext()
 
   const handleClick = () => {
-    const product = {
-      productId: id,
-      productTitle: title,
-      productImg: img_url,
-      productPrice: Number(steps),
-      productQuantity: 1,
-    }
-
-    setCart(prev => ({
-      totalQuantity: prev.totalQuantity + product.productQuantity,
-      totalPrice: prev.totalPrice + product.productPrice,
-      products: [...prev.products, product],
-    }))
+    addProductToCart(productDetails)
   }
 
-  const findProduct = cart.products.find(p => p.productId === id)
+  const findProduct = cart.products.find(p => p.productId === productDetails.id)
 
   if (findProduct) return <CartButton />
 
   return (
     <div className="sticky bottom-0 flex justify-between items-center border-primary border-t-[1px] p-3 mt-5 md:mt-10 bg-black shadow-top">
-      <h2>$ {steps}</h2>
+      <h2>$ {productDetails.steps}</h2>
       <button
         className="bg-primary w-36 p-2 rounded-full self-center"
         onClick={handleClick}
