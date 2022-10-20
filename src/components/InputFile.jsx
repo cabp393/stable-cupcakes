cbf229f05e26a38ac188d29d24e285dcb5c501fd
Camilo Alfonso <cabp393@gmail.com>
2022-10-20 02:19:33 -0300

    Change extract services and utilities

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { supabase } from '../lib/supabase'
-import { supabaseStorage } from '../lib/constants'
 import { IconDelete } from '../components/IconDelete'
 import toast from 'react-hot-toast'
+import uploadImage from '../services/uploadImage'
 import getImageDimensions from '../utils/getImageDimensions'
+import generateRandomPath from '../utils/generateRandomPath'
 
 export function InputFile({ productImage, setProductImage }) {
   const [uploading, setUploading] = useState(false)
@@ -13,21 +13,13 @@ export function InputFile({ productImage, setProductImage }) {
       setUploading(true)
 
       if (!e.target.files || e.target.files.length === 0) {
-        throw new error('You must select an image to upload.')
+        throw new error()
       }
 
       const file = e.target.files[0]
-      const fileExt = file.name.split('.').pop()
-      const fileName = `${Math.random()}.${fileExt}`
-      const filePath = `${fileName}`
+      const filePath = generateRandomPath(file)
+      const img_url = await uploadImage(filePath, file)
 
-      let { data, error: uploadError } = await supabase.storage
-        .from('products')
-        .upload(filePath, file)
-
-      if (uploadError) throw uploadError
-
-      const img_url = `${supabaseStorage}${data.path}`
       getImageDimensions(img_url, setProductImage)
       toast.success('image uploaded')
     } catch (error) {
@@ -37,36 +29,36 @@ export function InputFile({ productImage, setProductImage }) {
       setUploading(false)
     }
   }
+
+  if (productImage)
+    return (
+      <div className="flex justify-center relative">
+        <IconDelete
+          size={30}
+          onClick={() => setProductImage(null)}
+          className="absolute right-[10px] lg:right-[180px] -top-5 hover:cursor-pointer"
+        />
+        <img
+          src={productImage.img_url}
+          height={productImage.img_height}
+          width={productImage.img_width}
+          alt="Product image"
+          className="rounded-2xl object-contain max-h-64 self-center"
+        />
+      </div>
+    )
+
   return (
-    <>
-      {productImage?.img_url ? (
-        <div className="flex justify-center relative">
-          <IconDelete
-            size={30}
-            onClick={() => setProductImage(null)}
-            className="absolute right-[10px] lg:right-[180px] -top-5 hover:cursor-pointer"
-          />
-          <img
-            src={productImage.img_url}
-            height={productImage.img_height}
-            width={productImage.img_width}
-            alt="Product image"
-            className="rounded-2xl object-contain max-h-64 self-center"
-          />
-        </div>
-      ) : (
-        <input
-          type="file"
-          name="file"
-          accept="image/*"
-          onChange={uploadProductImg}
-          disabled={uploading}
-          className="max-w-[80vw] self-center file:mr-4 file:py-2 file:px-4
+    <input
+      type="file"
+      name="file"
+      accept="image/*"
+      onChange={uploadProductImg}
+      disabled={uploading}
+      className="max-w-[80vw] self-center file:mr-4 file:py-2 file:px-4
         file:rounded-full file:border-0
         file:text-sm file:font-semibold
         file:bg-primary file:text-white hover:file:cursor-pointer"
-        />
-      )}
-    </>
+    />
   )
 }
