fd827ad937346113edbd8d96111d2dbb512e4f47
Camilo Alfonso <cabp393@gmail.com>
2022-10-20 02:18:32 -0300

    Change extract logic

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,85 +1,50 @@
 import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import { useDataContext } from '../hooks/useDataContext'
 
 export function CartProduct({ product }) {
-  const { setCart } = useDataContext()
   const [inputQuantity, setInputQuantity] = useState(product.productQuantity)
+
+  const { removeProductFromCart, updateProductQuantityFromCart } =
+    useDataContext()
 
   const handleQuantity = e => {
     const value = e.target.value
     setInputQuantity(value)
-
-    setCart(prev => {
-      let findProduct = null
-      const filteredList = prev.products.filter(p => {
-        if (p.productId === product.productId) findProduct = p
-        return p.productId !== product.productId
-      })
-
-      const updateProduct = {
-        ...findProduct,
-        productQuantity: value,
-      }
-
-      const updateTotalPrice =
-        updateProduct.productQuantity * updateProduct.productPrice -
-        findProduct.productQuantity * findProduct.productPrice
-
-      const updateTotalQuantity =
-        updateProduct.productQuantity - findProduct.productQuantity
-
-      return {
-        ...prev,
-        totalQuantity: prev.totalQuantity + updateTotalQuantity,
-        totalPrice: prev.totalPrice + updateTotalPrice,
-        products: [...filteredList, updateProduct],
-      }
-    })
+    updateProductQuantityFromCart(product.productId, Number(value))
   }
 
   const handleRemove = () => {
-    setCart(prev => {
-      let findProduct = null
-      const removeProduct = prev.products.filter(p => {
-        if (p.productId === product.productId) findProduct = p
-        return p.productId !== product.productId
-      })
-
-      const updateTotalPrice =
-        findProduct.productPrice * product.productQuantity
-
-      return {
-        ...prev,
-        totalQuantity: prev.totalQuantity - findProduct.productQuantity,
-        totalPrice: prev.totalPrice - updateTotalPrice,
-        products: removeProduct,
-      }
-    })
+    removeProductFromCart(product.productId)
   }
 
   return (
     <article className="grid grid-cols-[20%_1fr_20%] gap-5 p-4 lg:px-80">
-      <img
-        src={product.productImg}
-        alt=""
-        className="rounded-[10px] max-w-40 lg:max-h-40"
-      />
+      <NavLink to={`/products/${product.productSlug}`}>
+        <img
+          src={product.productImg}
+          alt={product.productTitleTitle}
+          className="rounded-[10px] max-w-40 lg:max-h-40"
+        />
+      </NavLink>
 
       <div className="flex flex-col gap-2">
-        <div>{product.productTitle}</div>
+        <NavLink to={`/products/${product.productSlug}`}>
+          <div>{product.productTitle}</div>
+        </NavLink>
         <div className="text-content">$ {product.productPrice}</div>
       </div>
 
       <div className="flex flex-col items-center gap-2">
         <input
-          type="tel"
+          type="number"
           min={1}
           value={inputQuantity}
           onChange={handleQuantity}
-          className="rounded w-[50%] text-black text-center"
+          className="rounded w-[50%] text-black text-center md:pl-3"
         />
         <div
-          className="underline underline-offset-4 text-detail"
+          className="underline underline-offset-4 text-detail hover:cursor-pointer"
           onClick={handleRemove}
         >
           remove
