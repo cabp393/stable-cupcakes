8ca2a2d1282a6c3d83a86a08bd86061cdb6b892c
Camilo Alfonso <cabp393@gmail.com>
2022-10-21 07:39:42 -0300

    Add calculate totals

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,26 +1,28 @@
 import { Btn } from '../components/Btn'
 import { CartProduct } from '../components/CartProduct'
 import { useDataContext } from '../hooks/useDataContext'
+import { calculateTotals } from '../utils/calculateTotals'
 
 function Cart() {
   const { cart } = useDataContext()
+  const { totalPrice } = calculateTotals(cart)
 
   return (
     <section>
-      <div className="text-5xl text-center my-7 md:my-10 px-4">Your cart</div>
+      <h1 className="text-5xl text-center my-7 md:my-10 px-4">your cart</h1>
 
-      {cart.products.map(productItem => (
-        <CartProduct product={productItem} key={productItem.productId} />
+      {cart.map(p => (
+        <CartProduct product={p} key={p.id} />
       ))}
 
       <div className="flex flex-col mx-4 my-8 rounded lg:mx-80">
         <div className="border-t-detail border-t-[1px] pb-4" />
 
         <div className="flex justify-between text-detail ">
-          subtotal <span>$ {cart.totalPrice}</span>
+          subtotal <span>$ {totalPrice}</span>
         </div>
         <div className="flex justify-between">
-          total <span>$ {cart.totalPrice}</span>
+          total <span>$ {totalPrice}</span>
         </div>
         <Btn title={'checkout'} className="text-xl" />
       </div>
